95ce13a8c2441e7b688f9eb700f70bda2cedd52a
Arshjot Ghuman <arshjots11@gmail.com>
2023-11-03 12:05:25 -0500

    Add error for missing info

--- src/components/LoginCluster.tsx
+++ src/components/LoginCluster.tsx
@@ -17,47 +17,50 @@ export function LoginCluster() {
   const [email, setEmail] = useState(localStorage.getItem("user-email") || ""); // User email - always retrieve from localStorage if possible
   const [password, setPassword] = useState(""); // User password
   const [remember, setRemember] = useState(false); // Remember-me button state
+  const [formSubmitted, setFormSubmitted] = useState(false);
   const { login, error } = useLogin();
 
   // Handle login button
-  const handleSubmit = async (e: React.SyntheticEvent) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    await login(email, password); // Call API endpoint
-
-    // Store user email in local storage as "user-email"
-    if (remember && !error) {
-      localStorage.setItem("user-email", email);
-    } else {
-      localStorage.removeItem("user-email");
+    setFormSubmitted(true); // Mark the form as submitted
+    if (email && password) {
+      await login(email, password);
+      if (remember && !error) {
+        localStorage.setItem("user-email", email);
+      } else {
+        localStorage.removeItem("user-email");
+      }
     }
   };
 
   // Handle remember-me checkbox
-  const handleToggleRemember = (e: React.SyntheticEvent) => {
-    e.preventDefault();
+  const handleToggleRemember = () => {
     setRemember(!remember);
   };
 
   return (
-    <Form className="login-cluster" onSubmit={handleSubmit}>
+    <Form className={`login-cluster ${formSubmitted ? "form-submitted" : ""}`} onSubmit={handleSubmit}>
       {/* Email input */}
-      <Form.Group className="mb-3" controlId="formGroupEmail">
+      <Form.Group className={`mb-3`} controlId="formGroupEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control
-          type="email"
-          placeholder="Enter email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
+            className={`${formSubmitted && !password ? "no-info-error" : ""}`}
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
         />
       </Form.Group>
 
       {/* Password input */}
-      <Form.Group className="mb-3" controlId="formGroupPassword">
+      <Form.Group className={`mb-3`} controlId="formGroupPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
+            className={`${formSubmitted && !password ? "no-info-error" : ""}`}
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
         />
       </Form.Group>
 
