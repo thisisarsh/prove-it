77b614d3e798753518a5129e1e949de0c8498ab4
Arshjot Ghuman <arshjots11@gmail.com>
2023-11-03 21:57:51 -0500

    Add error messages

--- src/components/SignUpCluster.tsx
+++ src/components/SignUpCluster.tsx
@@ -20,13 +20,42 @@ export function SignUpCluster() {
     const [password, setPassword] = useState(""); // User password
     const [confirmPassword, setConfirmPassword] = useState(""); // User confirm password
     const { signup, error, isLoading } = useSignUp();
-    const [formSubmitted, setFormSubmitted] = useState(false);
+    const [passwordStrengthError, setError] = useState("");
 
     // Handle signup button
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, setter: React.Dispatch<React.SetStateAction<string>>) => {
+        setter(e.target.value);
+    };
+
+    const isPasswordStrong = (password: string): boolean => {
+        const passwordRegex = /^(?=.*[A-Z])(?=.*[@$!%*?&]).{8,14}$/;
+        return passwordRegex.test(password);
+    };
+
+    const clearErrors = () => {
+        setError("");
+    };
+
     const handleSubmit = async (e: React.SyntheticEvent) => {
         e.preventDefault();
-        setFormSubmitted(true); // Mark the form as submitted
-        await signup(lastName, firstName, email, password, confirmPassword); // Call API endpoint
+        clearErrors();
+
+        if (!firstName || !lastName || !email || !password || !confirmPassword) {
+            setError('All fields must be filled out.');
+            return;
+        }
+
+        if (password !== confirmPassword) {
+            setError('Password and Confirm Password do not match.');
+            return;
+        }
+
+        if (!isPasswordStrong(password)) {
+            setError('Password must be 8-14 characters long with 1 special character and 1 uppercase letter.');
+            return;
+        }
+
+        await signup(lastName, firstName, email, password); // Call API endpoint
 
         // Store user email, first namd and last name in local storage
         if (!error) {
@@ -42,68 +71,33 @@ export function SignUpCluster() {
 
     return (
         <Form className="login-cluster" onSubmit={handleSubmit}>
-            {/* first name input */}
-            <Form.Group className="mb-3" controlId="formGroupFirstName">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Enter First Name"
-                    onChange={(e) => setFirstName(e.target.value)}
-                    value={firstName}
-                />
-            </Form.Group>
-
-            {/* last name input */}
-            <Form.Group className="mb-3" controlId="formGroupLastName">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Enter Last Name"
-                    onChange={(e) => setLastName(e.target.value)}
-                    value={lastName}
-                />
-            </Form.Group>
-
-            {/* Email input */}
-            <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                />
-            </Form.Group>
-
-            {/* Password input */}
-            <Form.Group className="mb-3" controlId="formGroupPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </Form.Group>
-
-            {/* Password Confirm input */}
-            <Form.Group className="mb-3" controlId="formGroupPassword">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    placeholder="Confirm Password"
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-            </Form.Group>
-
-            {/* Buttons */}
+            <FormGroup label="First Name" value={firstName} onChange={(e) => handleInputChange(e, setFirstName)} />
+            <FormGroup label="Last Name" value={lastName} onChange={(e) => handleInputChange(e, setLastName)} />
+            <FormGroup label="Email address" type="email" value={email} onChange={(e) => handleInputChange(e, setEmail)} />
+            <FormGroup label="Password" type="password" value={password} onChange={(e) => handleInputChange(e, setPassword)} />
+            <FormGroup label="Confirm Password" type="password" value={confirmPassword} onChange={(e) => handleInputChange(e, setConfirmPassword)} />
+            {passwordStrengthError && <div className="error">{passwordStrengthError}</div>}
             <div className="sign-up-button-container">
-                <Button
-                    type="submit"
-                    className="sign-up-button standard-button"
-                >
+                <Button type="submit" className="sign-up-button standard-button">
                     Sign Up
                 </Button>
             </div>
         </Form>
+    );
+}
+
+interface FormGroupProps {
+    label: string;
+    type?: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function FormGroup({ label, type = 'text', value, onChange }: FormGroupProps) {
+    return (
+        <Form.Group className="mb-3" controlId={`formGroup${label.replace(/\s+/g, '')}`}>
+            <Form.Label>{label}</Form.Label>
+            <Form.Control type={type} placeholder={`Enter ${label}`} value={value} onChange={onChange} />
+        </Form.Group>
     );
 }
--- src/hooks/useSignup.tsx
+++ src/hooks/useSignup.tsx
@@ -13,42 +13,30 @@ export function useSignUp() {
         lastName: string,
         email: string,
         password: string,
-        confirmPassword: string,
     ) => {
         setIsLoading(true);
         setError(null);
         console.log(isLoading);
 
-        //check password and confirm password are the same and not empty
-        if (password != confirmPassword) {
-            alert("Password do not match");
-            setIsLoading(false);
-        } else if (password == "") {
-            //check empty password
-            alert("Password cannot be empty");
-            setIsLoading(false);
-        } else {
-            // API call
-            const response = await fetch(SIGNUP_API, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    xck: import.meta.env.VITE_HT_API_KEY,
-                },
-                body: JSON.stringify({
-                    user: { firstName, lastName, email, password },
-                }),
-            });
-            const json = await response.json();
+        const response = await fetch(SIGNUP_API, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                xck: import.meta.env.VITE_HT_API_KEY,
+            },
+            body: JSON.stringify({
+                user: { firstName, lastName, email, password },
+            }),
+        });
+        const json = await response.json();
 
-            // Handle BAD/GOOD response
-            if (json.isSuccess == false) {
-                setIsLoading(false);
-                setError(json.error);
-            } else if (json.isSuccess == true) {
-                setIsLoading(false);
-                navigate("/Login");
-            }
+        // Handle BAD/GOOD response
+        if (json.isSuccess == false) {
+            setIsLoading(false);
+            setError(json.error);
+        } else if (json.isSuccess == true) {
+            setIsLoading(false);
+            navigate("/Login");
         }
     };
 
