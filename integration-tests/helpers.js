fb1325627b26999b59cb94b281766004232ff33f
Arshjot Ghuman <arshjots11@gmail.com>
2023-11-03 20:59:19 -0500

    Babel fix

--- integration-tests/helpers.js
+++
@@ -1,9 +0,0 @@
-import webdriver from 'selenium-webdriver';
-import chrome from 'chromedriver';
-
-export const getDriver = () => {
-    let driver = new webdriver.Builder()
-        .forBrowser('chrome')
-        .build();
-    return driver;
-};
