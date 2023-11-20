d63e3d5946f3a5a82883d92d67a7ebca77b11f57
ACS4901 Group1 <acs4901group1@gmail.com>
2023-11-20 21:38:45 +0000

    Update useLogin.tsx

--- frontend/src/hooks/useLogin.tsx
+++ frontend/src/hooks/useLogin.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
 import { useNavigate } from "react-router-dom";
 
-const LOGIN_LINK = "http://ht-web-server-dev:5000/login";
+const LOGIN_LINK = "http://ht-web-server-dev-ht-uat-coop.apps.prod.htuslab2.com/login";
 
 export function useLogin() {
     const [error, setError] = useState(null);
